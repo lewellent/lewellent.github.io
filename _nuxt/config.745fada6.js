46a5a12594208a8b4f420d688db2f2e9caa64d15
Todd Lewellen <todd@iso.io>
2023-09-06 10:31:16 -0400

    Updates

--- _nuxt/config.745fada6.js
+++
@@ -1,1 +0,0 @@
-import{a9 as p,aa as o,M as a}from"./entry.e98381d6.js";const i={},f=p(i);function s(){const n=a();return n._appConfig||(n._appConfig=o(f)),n._appConfig}export{s as u};
---
+++ assets/DocumentDrivenNotFound.67c2c5d0.js
@@ -0,0 +1,1 @@
+import{g as n,N as e}from"./entry.1a0efb20.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
